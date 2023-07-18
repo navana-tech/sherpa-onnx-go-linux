3d96d8c94be285509580491c1ccc163c565a714d
Fangjun Kuang <csukuangfj@gmail.com>
2023-07-18 17:33:56 +0800

    First commit

---
+++ sherpa_onnx/build_amd64.go
@@ -0,0 +1,6 @@
+//go:build !android && linux && amd64 &&!musl
+
+package sherpa_onnx
+
+// #cgo LDFLAGS: -L ${SRCDIR}/../lib/x86_64-linux-gnu -lsherpa-onnx-c-api -lsherpa-onnx-core -lkaldi-native-fbank-core -lonnxruntime -Wl,-rpath,${SRCDIR}/../lib/x86_64-linux-gnu
+import "C"
