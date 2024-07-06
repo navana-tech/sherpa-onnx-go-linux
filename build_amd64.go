414ad0fbf154d5a27a73b896e74beefe0f988142
Fangjun Kuang <csukuangfj@gmail.com>
2024-07-06 05:56:32 +0000

    Release v1.10.9

--- build_amd64.go
+++
@@ -1,6 +0,0 @@
-//go:build !android && linux && amd64 && !musl
-
-package sherpa_onnx
-
-// #cgo LDFLAGS: -L ${SRCDIR}/lib/x86_64-unknown-linux-gnu -lsherpa-onnx-c-api -lsherpa-onnx-core -lkaldi-native-fbank-core  -lkaldi-decoder-core -lsherpa-onnx-kaldifst-core -lsherpa-onnx-fstfar -lsherpa-onnx-fst -lpiper_phonemize -lespeak-ng -lucd -lonnxruntime -lssentencepiece_core -Wl,-rpath,${SRCDIR}/lib/x86_64-unknown-linux-gnu
-import "C"
--- build_arm.go
+++
@@ -1,6 +0,0 @@
-//go:build linux && arm && !arm7
-
-package sherpa_onnx
-
-// #cgo LDFLAGS: -L ${SRCDIR}/lib/arm-unknown-linux-gnueabihf -lsherpa-onnx-c-api -lsherpa-onnx-core -lkaldi-native-fbank-core  -lkaldi-decoder-core -lsherpa-onnx-kaldifst-core -lsherpa-onnx-fstfar -lsherpa-onnx-fst -lpiper_phonemize -lespeak-ng -lucd -lonnxruntime -lssentencepiece_core -Wl,-rpath,${SRCDIR}/lib/arm-unknown-linux-gnueabihf
-import "C"
